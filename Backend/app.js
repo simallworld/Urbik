9c5ae650968de0c8e47a8329be6e89d424b16b9f
Mitesh Verma <miteshverma989@gmail.com>
2025-06-23 17:37:46 +0530

    Created Server

---
+++ Backend/app.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+
+
+
+
+app.get('/',  (req, res) => {
+  res.send ('Hi There');
+});
+
+module.export = app;
