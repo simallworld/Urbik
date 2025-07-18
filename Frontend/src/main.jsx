32efc8484fb1b6a4d24e6c8554a84712d71bb0bc
Shivam Swaroop <shivamswaroop89@gmail.com>
2025-07-19 02:36:16 +0530

    Fix: Case-sensitive import path for userContext

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router";
 import './index.css'
 import App from './App.jsx'
-import UserContext from './context/userContext.jsx';
+import UserContext from './context/UserContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
